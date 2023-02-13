2adb7cbee28dcc621cfcb947c84de62152f3d2bb
Drew <drc920630@gmail.com>
2023-02-14 02:02:59 +0900

    달력 돌아버리겠네

--- class2/finalReport/wedding2/static/js.js
+++ class2/finalReport/wedding2/static/js.js
@@ -1,18 +1,27 @@
 let day = '';
+let date = new Date("2023-05");
+let toYear = date.getFullYear();
+let toMonth = date.getMonth();
+let firstDay = new Date(toYear, toMonth, 1).getDay();
+let lastDate = new Date(toYear, toMonth + 1, 0).getDate();
+let lastDay = new Date(toYear, toMonth, lastDate).getDay();
+console.log(firstDay);
+console.log(lastDay);
+console.log(lastDate);
 
-for (let i = 0; i < 32; i += 1){
-  let date = new Date(`2023-05-${i}`).getDay();
-  if (date === 0) {
-    day += "[]";
-  } else {
-    day += " " + i;
-  }
+let month = `${toYear}년 ${toMonth + 1}입니다.`
+console.log(month);
 
-  if (i !== 0 && i % 7 === 0) {
-    day += "\n";
-  }
-  
+for (let bin = 0; bin < firstDay; bin += 1) {
+  day += "[ ]";
+  document.querySelector("#main__calender tr").innerHTML = "<td>${day}</td>";
 }
 
-day[0] = " ";
+for (let i = 1; i <= lastDate; i += 1) {
+  day += " " + i;
+  if (i % 7 === 0) {
+    day += "\n"; 
+  } 
+}
+
 console.log(day);
